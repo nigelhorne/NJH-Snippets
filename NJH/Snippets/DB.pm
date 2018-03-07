487c2ca16e9041883e8a00135d54dfd56e67c0a4
Nigel Horne <njh@bandsman.co.uk>
2018-03-07 14:27:24 -0500

    Noted a new TODO

--- NJH/Snippets/DB.pm
+++ NJH/Snippets/DB.pm
@@ -10,6 +10,8 @@ package NJH::Snippets::DB;
 #	must apply in writing for a licence for use from Nigel Horne at the
 #	above e-mail.
 
+# TODO: support a directory hierachy of databases
+
 # Abstract class giving read-only access to CSV, XML and SQLite databases
 
 # You can then access the files in $directory/foo.csv via this class:
