7b1f05a19a0a6407f6929b0c147ad3f998484eb2
Nigel Horne <njh@bandsman.co.uk>
2023-12-22 08:39:06 -0800

    Added TODO comment

--- bin/twitter_status.py
+++ bin/twitter_status.py
@@ -5,6 +5,7 @@ import sys
 
 scraper = Nitter(log_level=0, skip_instance_check=False)
 
-bbportal_tweets = scraper.get_tweets(sys.argv[1], mode='user', since='2023-12-20')
+# TODO: work out how to say a week ago
+bbportal_tweets = scraper.get_tweets(sys.argv[1], mode='user', since='2023-12-17')
 
 print(bbportal_tweets)
