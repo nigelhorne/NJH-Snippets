a983b4b067987c163bf7bbcfc4ff7748e252a6ae
Nigel Horne <njh@bandsman.co.uk>
2023-12-22 11:33:57 -0800

    Use the python to produce JSON rather dict

--- bin/twitter_status.py
+++ bin/twitter_status.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python3
+
+# Show the tweets from a given account over the last week
 # https://github.com/bocchilorenzo/ntscraper
 
 from ntscraper import Nitter
+import datetime
+import json
 import sys
 
 scraper = Nitter(log_level=0, skip_instance_check=False)
 
-# TODO: work out how to say a week ago
-bbportal_tweets = scraper.get_tweets(sys.argv[1], mode='user', since='2023-12-17')
+timestamp = datetime.datetime.now() - datetime.timedelta(days=7)
 
-print(bbportal_tweets)
+bbportal_tweets = scraper.get_tweets(sys.argv[1], mode='user', since=timestamp.strftime('%F'))
+
+print(json.dumps(bbportal_tweets))
