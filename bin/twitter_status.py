1b08074ec304f952a408ac06e963f3e1b5954d1b
Nigel Horne <njh@bandsman.co.uk>
2023-12-22 07:09:12 -0800

    Rename to twitter_status

--- bin/twitter_status.py
+++ bin/twitter_status.py
@@ -1,9 +1,10 @@
 # https://github.com/bocchilorenzo/ntscraper
 
 from ntscraper import Nitter
+import sys
 
 scraper = Nitter(log_level=0, skip_instance_check=False)
 
-bbportal_tweets = scraper.get_tweets('bbportal', mode='user', since='2023-12-20')
+bbportal_tweets = scraper.get_tweets(sys.argv[1], mode='user', since='2023-12-20')
 
 print(bbportal_tweets)
