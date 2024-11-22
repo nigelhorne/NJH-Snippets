80125039983cbdf7b5692e3df88d70aa73863ad0
Nigel Horne <njh@bandsman.co.uk>
2024-11-22 15:24:02 -0500

    Make midi.pl a little easier to read

--- bin/midi.pl
+++ bin/midi.pl
@@ -10,20 +10,21 @@ foreach my $file (@ARGV) {
 }
 
 # Subroutine to process a MIDI file and print specific text events
-sub process_midi_file {
+sub process_midi_file
+{
 	my($file) = @_;
 
 	# Create a new MIDI Opus object from the file
 	my $opus = MIDI::Opus->new({ 'from_file' => $file, 'no_parse' => 0 });
 
 	# Iterate through each track in the MIDI file
-	for my $track ($opus->tracks) {
+	for my $track ($opus->tracks()) {
 		# Iterate through each event in the track
-		for my $event ($track->events) {
-			my($type, undef, $text) = @$event;
+		for my $event ($track->events()) {
+			my($type, undef, $text) = @{$event};
 
 			# Check for text or name events and print non-empty text
-			if($type =~ /^(text|name)$/ && defined $text && $text ne '') {
+			if(($type =~ /^(text|name)$/) && (defined $text) && ($text ne '')) {
 				print "$text\n";
 			}
 		}
