64ebb3d3074e9251d462580fbde75ae3793a8164
Nigel Horne <njh@bandsman.co.uk>
2024-11-22 15:22:56 -0500

    Added bin/midi.pl

---
+++ bin/midi.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use MIDI;
+
+# Process each file provided as an argument
+foreach my $file (@ARGV) {
+	process_midi_file($file);
+}
+
+# Subroutine to process a MIDI file and print specific text events
+sub process_midi_file {
+	my($file) = @_;
+
+	# Create a new MIDI Opus object from the file
+	my $opus = MIDI::Opus->new({ 'from_file' => $file, 'no_parse' => 0 });
+
+	# Iterate through each track in the MIDI file
+	for my $track ($opus->tracks) {
+		# Iterate through each event in the track
+		for my $event ($track->events) {
+			my($type, undef, $text) = @$event;
+
+			# Check for text or name events and print non-empty text
+			if($type =~ /^(text|name)$/ && defined $text && $text ne '') {
+				print "$text\n";
+			}
+		}
+	}
+}
